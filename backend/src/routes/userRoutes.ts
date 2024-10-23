31e0f22fd0d3eb38ea395fbf7d29f17dc0153373
Kacper Matlag <matlagkacper@gmail.com>
2024-10-23 15:23:40 +0200

    Add repositories folder

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,45 +1,9 @@
-import { Router, Response, Request } from "express";
-import { User } from "../models/user";
+import { Router } from "express";
+import { getUserById, registerUser } from "../repositories/userRepository";
 
 const router = Router();
 
-router.post("/register", async (req: Request, res: Response) => {
-  try {
-    const { email } = req.body;
-    const findUserByEmail = await User.findOne({
-      where: {
-        email: email,
-      },
-    });
-
-    if (findUserByEmail) {
-      res.status(409).json({ message: "Adres email jest juz zajety" });
-      return;
-    }
-
-    await User.create(req.body);
-    const { password, ...userWithoutPassword } = req.body;
-    res.status(201).json(userWithoutPassword);
-  } catch (error) {
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
-
-router.get("/:id", async (req: Request, res: Response) => {
-  try {
-    const user = await User.findByPk(req.params.id, {
-      attributes: { exclude: ["password"] },
-    });
-
-    if (!user) {
-      res.status(404).json({ message: "User not found" });
-      return;
-    }
-
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
+router.post("/register", registerUser);
+router.get("/:id", getUserById);
 
 export default router;
