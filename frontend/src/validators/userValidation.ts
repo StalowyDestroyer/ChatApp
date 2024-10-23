78553bb30732a30e54582a5df53aa21d79c98b93
Kacper Matlag <matlagkacper@gmail.com>
2024-10-23 15:31:57 +0200

    Change abort early to true in register validation

--- frontend/src/validators/userValidation.ts
+++ frontend/src/validators/userValidation.ts
@@ -2,6 +2,11 @@ import * as yup from "yup";
 import { UserFormData } from "../types/types";
 
 const registerSchema = yup.object().shape({
+  username: yup
+    .string()
+    .required("Nazwa użytkownika jest wymagana")
+    .min(5, "Nazwa uzytkownika musi mieć minimum 5 znaków")
+    .max(30, "Nazwa użytkownika musi mieć poniżej 30 znaków"),
   email: yup
     .string()
     .required("Email jest wymagany")
@@ -21,16 +26,11 @@ const registerSchema = yup.object().shape({
       const { password } = this.parent;
       return password == value;
     }),
-  username: yup
-    .string()
-    .required("Nazwa użytkownika jest wymagana")
-    .min(5, "Nazwa uzytkownika musi mieć minimum 5 znaków")
-    .max(30, "Nazwa użytkownika musi mieć poniżej 30 znaków"),
 });
 
 export const validateUserForRegister = async (data: UserFormData) => {
   try {
-    await registerSchema.validate(data, { abortEarly: false });
+    await registerSchema.validate(data, { abortEarly: true });
     return true;
   } catch (error) {
     if (error instanceof yup.ValidationError) console.log(error.errors);
